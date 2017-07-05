db281ca0c324a8e8241e1c38779877385bae3ad2
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-07-04 22:51:08 -0700

    Get port from environment

--- interface-server.js
+++ interface-server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.post('/path', function(req, res) {
   res.send('Path received. ');
 });
 
-app.listen(3000, function() {
-  console.log('Server running on port 3000. ');
+app.listen(app.get('port'), function() {
+  console.log('Server running on port ', app.get('port'));
 });
