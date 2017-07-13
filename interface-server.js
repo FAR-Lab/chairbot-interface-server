de10c68fad68f3c6523e2b20963fbedf0fb672b9
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-07-13 12:31:16 -0700

    Some path creation progress

--- interface-server.js
+++ interface-server.js
@@ -1,15 +1,27 @@
 // Express application to receive paths from iPad
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    yaml = require('write-yaml');
 
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.post('/path', function(req, res) {
-  console.log(req.body);
 
   // Do something with req.body.path here
   // Convert to YAML file?
+
+  var path = [];
+  req.body.path.forEach(function(element) {
+    var current = { x: element[0], y: element[1] };
+    path.push(current);
+  });
+  console.log(path);
+
+  var data = { features: path };
+
+  yaml.sync('../path_0.yml', data);
+
   res.send({ status: 'SUCCESS' });
 });
 app.listen(app.get('port'), function() {
